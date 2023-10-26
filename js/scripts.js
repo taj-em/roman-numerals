ef632c8765d5cedee068e9cf6180603859d593d5
Your Name <tajempey@gmail.com>
2023-10-26 14:16:25 -0600

    Add UI, refactor code

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,10 @@
-
-function assignOnes(array, number) {
+function assignNumeral(array, number) {
   for (let index = 0; index < array.length; index += 1) {
-    if (index == number) {
-      number = array[index];
-      return number;
-    }
-  };
-}
-function assignTens(array, number) {
-  for (let index = 0; index < array.length; index += 1) {
-    if (index == number) {
-      number = array[index];
-      return number;
-    }
-  };
-}
-function assignHundreds(array, number) {
-  for (let index = 0; index < array.length; index += 1) {
-    if (index == number) {
-      number = array[index];
-      return number;
-    }
-  };
-}
-function assignThousands(array, number) {
-  for (let index = 0; index < array.length; index += 1) {
-    if (index == number) {
-      number = array[index];
-      return number;
+    if (number === undefined) {
+      return "";
+    } else if (index == number) {
+      let numeral = array[index];
+      return numeral;
     }
   };
 }
@@ -35,27 +12,41 @@ function assignThousands(array, number) {
 
 function convertToNumeral(num) {
   const onesArray = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
-  const tensArray = ["", "X", "XX", "XXX", "IV", "L", "LX", "LXX", "LXXX", "XC"]
+  const tensArray = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
   const hundredsArray = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
   const thousandsArray = ["", "M", "MM", "MMM"]
   const errorMSG = "Please Enter A Valid Number";
-  if (num === undefined) {
-    return errorMSG;
-  }
   const numArray = num.toString().split("")
   const flipArray = numArray.slice().reverse();
   let finalArray = [];
-  let ones = assignOnes(onesArray, flipArray[0]);
-  let tens = assignTens(tensArray, flipArray[1]);
-  let hundreds = assignHundreds(hundredsArray, flipArray[2]);
-  let thousands = assignThousands(thousandsArray, flipArray[3]);
+  let ones = assignNumeral(onesArray, flipArray[0]);
+  let tens = assignNumeral(tensArray, flipArray[1]);
+  let hundreds = assignNumeral(hundredsArray, flipArray[2]);
+  let thousands = assignNumeral(thousandsArray, flipArray[3]);
   console.log("Flip Array: " + flipArray);
-  if (flipArray[3] >= 4 || flipArray[0] !== Number) {
+  if (flipArray[3] >= 4) {
     return errorMSG;
   }
   finalArray.unshift(ones);
   finalArray.unshift(tens);
   finalArray.unshift(hundreds);
   finalArray.unshift(thousands);
-  return finalArray;
+  if (num < 0) {
+    return "-" + finalArray.join("")
+  }
+  return finalArray.join("");
 };
+
+function display(event) {
+event.preventDefault();
+const userInput = document.getElementById("text-box").value;
+const displayOutput = document.getElementById("display-numeral");
+const displayNumeral = convertToNumeral(userInput)
+displayOutput.innerText = "";
+displayOutput.append(displayNumeral);
+};
+
+
+window.addEventListener("load", function () {
+  document.querySelector("form").addEventListener("submit", display)
+});
